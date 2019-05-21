c9c52c421da00717f60de848aa0bcf81c1d77587
Bruno, Furukawa <furukawaborgesbruno@gmail.com>
2019-05-21 18:12:31 -0300

    Ajustando frontezinho

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component , OnInit} from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 declare var $: any;
 declare var jQuery: any;
@@ -9,10 +10,13 @@ declare var jQuery: any;
   styleUrls: ['./app.component.css']
 })
 export class AppComponent  implements OnInit {
+  public form: FormGroup;
   public produtos: any;
+  public formVisible = false;
+  public text: any;
   title = 'app';
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private fb: FormBuilder) {}
 
   ngOnInit() {
     this.getProdutos();
@@ -21,6 +25,7 @@ export class AppComponent  implements OnInit {
   getProdutos() {
     return this.http.get(`http://localhost:5001/produtos`)
     .subscribe((produtos: any) => {
+      produtos['isBOSTA'] = false;
       this.produtos = produtos.data;
     });
   }
@@ -35,11 +40,29 @@ export class AppComponent  implements OnInit {
   criar() {
     const data = {
       name: `Produto ${new Date()}`,
-      value: 99.88
+      value: Math.random() * 65536
     };
 
     return this.http.post(`http://localhost:5001/produtos`, data).subscribe(result => {
       console.log(result);
+      this.getProdutos();
+    });
+  }
+
+  send(produto) {
+    this.form = this.fb.group({
+      idProduto: [produto.id],
+      cep: [],
+      numero: [],
+      complemento: []
+    });
+    this.formVisible = this.formVisible ? false : true;
+  }
+  submit() {
+    $('.segment').dimmer('show');
+    return this.http.post(`http://localhost:5001/checkout`, this.form.value).subscribe(result => {
+      this.text = result;
+      $('.segment').dimmer('hide');
       this.getProdutos();
     });
   }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 
@@ -10,7 +11,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule, FormsModule, ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
